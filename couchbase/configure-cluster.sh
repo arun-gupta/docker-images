4417d4eeb4af20806ca0ec699a73cb1e52a5e2f2
Arun Gupta <arun.gupta@gmail.com>
2015-12-07 06:44:51 -0800

    starting work on a fully pre-configured Couchbase image

---
+++ couchbase/configure-cluster.sh
@@ -0,0 +1,9 @@
+/entrypoint.sh couchbase-server &
+
+sleep 10
+
+curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
+curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
+curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
+
