4e9d23639ff6d65985ec63e08692f61592d2451c
Arun Gupta <arun.gupta@gmail.com>
2015-12-07 18:15:30 -0800

    enabling job control

--- couchbase/configure-cluster.sh
+++ couchbase/configure-cluster.sh
@@ -1,3 +1,5 @@
+set -m
+
 /entrypoint.sh couchbase-server &
 
 sleep 10
@@ -6,4 +8,6 @@ curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexM
 curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
 curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
+
+fg 1
 
