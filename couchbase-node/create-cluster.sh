88033ea91ec9a37435064c48f1d92f75edaff636
Arun Gupta <arun.gupta@gmail.com>
2016-01-11 17:40:53 -0800

    Docker Swarm cluster is created, 3 Couchbase instances are started

--- couchbase-node/create-cluster.sh
+++ couchbase-node/create-cluster.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+#set -x
 
 # Docker Machine for Consul
 docker-machine \
-  create 
+  create \
   -d virtualbox \
   consul-machine
 
@@ -25,66 +26,78 @@ docker-machine \
   swarm-master
 
 # Docker Swarm node-01
-docker-machine /
-  create /
-  -d virtualbox /
-  --virtualbox-disk-size "5000" /
-  --swarm /
-  --swarm-discovery="consul://$(docker-machine ip consul-machine):8500" /
-  --engine-opt="cluster-store=consul://$(docker-machine ip consul-machine):8500" /
-  --engine-opt="cluster-advertise=eth1:2376" /
+docker-machine \
+  create \
+  -d virtualbox \
+  --virtualbox-disk-size "5000" \
+  --swarm \
+  --swarm-discovery="consul://$(docker-machine ip consul-machine):8500" \
+  --engine-opt="cluster-store=consul://$(docker-machine ip consul-machine):8500" \
+  --engine-opt="cluster-advertise=eth1:2376" \
   swarm-node-01
 
 # Docker Swarm node-02
-docker-machine /
-  create /
-  -d virtualbox /
-  --virtualbox-disk-size "5000" /
-  --swarm /
-  --swarm-discovery="consul://$(docker-machine ip consul-machine):8500" /
-  --engine-opt="cluster-store=consul://$(docker-machine ip consul-machine):8500" /
-  --engine-opt="cluster-advertise=eth1:2376" /
+docker-machine \
+  create \
+  -d virtualbox \
+  --virtualbox-disk-size "5000" \
+  --swarm \
+  --swarm-discovery="consul://$(docker-machine ip consul-machine):8500" \
+  --engine-opt="cluster-store=consul://$(docker-machine ip consul-machine):8500" \
+  --engine-opt="cluster-advertise=eth1:2376" \
   swarm-node-02
 
 # Configure to use Docker Swarm cluster
 eval "$(docker-machine env --swarm swarm-master)"
 
 # Create an overlay network
-docker /
-  network /
-  create /
-  -d overlay /
+docker \
+  network \
+  create \
+  -d overlay \
   couchbase-net
 
-# No docs on how to use port filter on overlay network?? 
-# TODO
+# Swarm will ensure that Couchbase is started on a node where port is available
 
-# Start one instance of Couchbase
-docker run -d --net=couchbase-net -p 8091-8093:8091-8093 -p 11210:11210 arungupta/couchbase-node
-
-# Start second instance of Couchbase
-docker run -d --net=couchbase-net -p 8091-8093:8091-8093 -p 11210:11210 arungupta/couchbase-node
-
-# Start third instance of Couchbase
-docker run -d --net=couchbase-net -p 8091-8093:8091-8093 -p 11210:11210 arungupta/couchbase-node
+# Start three instances of Couchbase
+for i in {1..3}
+do
+  docker \
+    run \
+    -d \
+    --net=couchbase-net \
+    -p 8091-8093:8091-8093 \
+    -p 11210:11210 \
+    arungupta/couchbase-node
+done
 
 # Get IP address of the container on swarm-master
-IP1=docker inspect --format '{{ .NetworkSettings.IPAddress }}' {CID}
+# declare -a ip
+# count=0
+# for i in `docker ps -q`
+# do
+#   ${ip[$count]}=`docker inspect --format '{{ index .NetworkSettings.Ports "8091/tcp" 0 "HostIp" }}' $i`
+#   ((count++))
+# done
 
-# Get IP address of the container on swarm-node-01
-IP2
+# echo ${ip[@]}
 
-# Get IP address of the container on swarm-node-02
-IP3
+# IP1=
 
-# Configure Couchbase cluster
-curl -X POST -u Administrator:password http://IP1:8091/controller/addNode -d hostname=IP2 -d user=Administrator -d password=password -d services=kv,n1ql,index -o O1
-{"otpNode":"ns_1@172.17.0.4"}
-curl -X POST -u Administrator:password http://IP1:8091/controller/addNode -d hostname=IP3 -d user=Administrator -d password=password -d services=kv,n1ql,index -o O2
-{"otpNode":"ns_1@172.17.0.5"}
+# # Get IP address of the container on swarm-node-01
+# IP2
 
-# Rebalance the cluster:
-curl -v -X POST -u Administrator:password http://IP1:8091/controller/rebalance --data 'knownNodes=ns_1%40172.17.0.3%2Cns_1%40172.17.0.4 %2Cns_1%40172.17.0.5&ejectedNodes='
+# # Get IP address of the container on swarm-node-02
+# IP3
 
+# # Configure Couchbase cluster
+# curl -X POST -u Administrator:password http://IP1:8091/controller/addNode -d hostname=IP2 -d user=Administrator -d password=password -d services=kv,n1ql,index -o O1
+# {"otpNode":"ns_1@172.17.0.4"}
+# curl -X POST -u Administrator:password http://IP1:8091/controller/addNode -d hostname=IP3 -d user=Administrator -d password=password -d services=kv,n1ql,index -o O2
+# {"otpNode":"ns_1@172.17.0.5"}
 
+#curl -X GET -u Administratator:password http://IP1:8091/pools/default 
+
+# # Rebalance the cluster:
+# curl -v -X POST -u Administrator:password http://IP1:8091/controller/rebalance --data 'knownNodes=ns_1%40172.17.0.3%2Cns_1%40172.17.0.4 %2Cns_1%40172.17.0.5&ejectedNodes='
 
