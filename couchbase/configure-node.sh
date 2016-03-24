755461105e92b1113e4d6020657afbd7774b402b
Arun Gupta <arun.gupta@gmail.com>
2016-03-24 14:22:21 -0700

    adding travel-sample bucket

--- couchbase/configure-node.sh
+++ couchbase/configure-node.sh
@@ -7,6 +7,7 @@ sleep 15
 curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
 curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
 
 echo "Type: $TYPE, Master: $COUCHBASE_MASTER"
 
