81bfc8e23d7acb5e9515a16a1d51b5f94015acfc
Arun Gupta <arun.gupta@gmail.com>
2016-06-27 20:13:49 -0700

    adding memory-optimized indexes default

--- couchbase/configure-node.sh
+++ couchbase/configure-node.sh
@@ -4,10 +4,20 @@ set -m
 
 sleep 15
 
+# Setup index and memory quota
 curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
+
+# Setup services
 curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
+
+# Setup credentials
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+
+# Load travel-sample bucket
 curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
+
+# Setup Memory Optimized Indexes
+curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
 
 echo "Type: $TYPE, Master: $COUCHBASE_MASTER"
 
