6380cfcbe176736cd5c8e3be9f988b6c9162b4ae
Celso Bem dos Santos <zedblade.santos@gmail.com>
2016-07-26 22:19:06 +0100

    Update configure-node.sh
    Change 127.0.0.1 addresses by dynamically fetched internal ip address.
    Right now, using 127 addresses fails on curl requests

--- couchbase/configure-node.sh
+++ couchbase/configure-node.sh
@@ -4,20 +4,22 @@ set -m
 
 sleep 15
 
+INTERNAL_IP=$(hostname -i)
+
 # Setup index and memory quota
-curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
+curl -v -X POST http://$INTERNAL_IP:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
 
 # Setup services
-curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
+curl -v http://$INTERNAL_IP:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
 
 # Setup credentials
-curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+curl -v http://$INTERNAL_IP:8091/settings/web -d port=8091 -d username=Administrator -d password=password
 
 # Setup Memory Optimized Indexes
-curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
+curl -i -u Administrator:password -X POST http://$INTERNAL_IP:8091/settings/indexes -d 'storageMode=memory_optimized'
 
 # Load travel-sample bucket
-curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
+#curl -v -u Administrator:password -X POST http://$INTERNAL_IP:8091/sampleBuckets/install -d '["travel-sample"]'
 
 echo "Type: $TYPE, Master: $COUCHBASE_MASTER"
 
