805aba22bf629d0718ce3afcd10e2fd633a5992d
Arun Gupta <arun.gupta@gmail.com>
2016-08-01 15:18:49 -0700

    cleaning up script

--- couchbase/configure-node.sh
+++ couchbase/configure-node.sh
@@ -19,21 +19,18 @@ curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes
 # Load travel-sample bucket
 curl -v -u Administrator:password -X POST http://127.0.0.1:8091/sampleBuckets/install -d '["travel-sample"]'
 
-echo "Type: $TYPE, Master: $COUCHBASE_MASTER"
+echo "Type: $TYPE
 
-if [ "$TYPE" = "worker" ]; then
+if [ "$TYPE" = "WORKER" ]; then
   sleep 15
-  set IP=`hostname -I`
-  couchbase-cli server-add --cluster=$COUCHBASE_MASTER:8091 --user Administrator --password password --server-add=$IP
-  # TODO: Hack with the cuts, use jq may be better.
-  #KNOWN_NODES=`curl -X POST -u Administrator:password http://$COUCHBASE_MASTER:8091/controller/addNode \
-  #  -d hostname=$IP -d user=Administrator -d password=password -d services=kv,n1ql,index | cut -d: -f2 | cut -d\" -f 2 | sed -e   's/@/%40/g'`
 
+  IP=`hostname -I`
+
+  echo "Auto Rebalance: $AUTO_REBALANCE"
   if [ "$AUTO_REBALANCE" = "true" ]; then
-    echo "Auto Rebalance: $AUTO_REBALANCE"
-    sleep 10
-    couchbase-cli rebalance -c $COUCHBASE_MASTER:8091 -u Administrator -p password --server-add=$IP
-    #curl -v -X POST -u Administrator:password http://$COUCHBASE_MASTER:8091/controller/rebalance --data "knownNodes=$KNOWN_NODES&ejectedNodes="
+    couchbase-cli rebalance --cluster=$COUCHBASE_MASTER:8091 --user=Administrator --password=password --server-add=$IP --server-add-username=Administrator --server-add-password=password
+  else
+    couchbase-cli server-add --cluster=$COUCHBASE_MASTER:8091 --user=Administrator --password=password --server-add=$IP --server-add-username=Administrator --server-add-password=password
   fi;
 fi;
 
